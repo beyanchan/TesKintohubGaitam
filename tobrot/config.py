d5bd6c403aaa396c0d5ee7a5f1dc9a7645467308
beyanchan <75328415+beyanchan@users.noreply.github.com>
2020-12-21 16:04:23 +0800

    Update config.py

--- tobrot/config.py
+++ tobrot/config.py
@@ -13,10 +13,7 @@ class Config(object):
   OWNER_ID = 1480779663
   RCLONE_CONFIG = """type = drive
 scope = drive
-root_folder_id = 1rWomzK1yVjkqIbC9p9EBBKJgcf3imGr4
-token = {"access_token":"ya29A0AfH6SMCaLwuWkNALk5I4IWBZP6y5Y4lZRkgepXoaf-LvSu2jmNDyLzezwJk-HlgcP1OSiRKqAa9qKsCBMv9O2IMd1ebAwhYdVoNP7jIXMRBDKzD10qfrzq7oGda_Z-vP3Qvl6PQcOYC3x5ecEkU3vMIyqEC2xgANsEnowpSENeU","token_type":"Bearer","refresh_token":"1//0gHp6knwySTm9CgYIARAAGBASNwF-L9IrRPO94mU1d9kBkUu1BbYrkA_EMhGXcCiHXGULiAZDg5BmaSxhrtALRfBmVpiotPEtK6Q","expiry":"2020-11-11T13:38:54.4701885+06:00"}
-team_drive = 1rWomzK1yVjkqIbC9p9EBBKJgcf3imGr4"""
-  DESTINATION_FOLDER = ""
+token = {"access_token":"ya29.a0AfH6SMB56uBYpLpBCTcERiRt8sRWuQ2H1Zgnei9UKndFCeeyCrXpWbhZuMi02BwyrypdW58wrjmv7PEhRFlYCfk_ewzL5pyuoeI0Hv4gg_NWzRFIBqGDHy6PVebZtLF8jy8yidDH1-R0pt_HZoOv6E_T35lz-Qq4kMXeMe1HrX8","token_type":"Bearer","refresh_token":"1//0gdVePwbuhacKCgYIARAAGBASNwF-L9IriD5ajDaKEEfzdz8oCgbBVOivVTP8TjVfAA5Sfk0Rqn4YhF1kX19Ov0HcW_laIHO5GkQ","expiry":"2020-12-21T09:01:22.303947129Z"}"""
   GLEECH_COMMAND = "gleech"
   TELEGRAM_LEECH_COMMAND_G = "tleech"
   PYTDL_COMMAND_G = "pytdl"
