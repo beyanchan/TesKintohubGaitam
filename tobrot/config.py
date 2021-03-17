50e0981f32ec8302a5f599d0cc1a57eaf847f09c
insania93 <75328415+insania93@users.noreply.github.com>
2021-03-17 16:54:51 +0800

    Update config.py

--- tobrot/config.py
+++ tobrot/config.py
@@ -1,20 +1,37 @@
-import os
+from tobrot.sample_config import Config
 
-class Config(object):
-  TG_BOT_TOKEN = "1457652942:AAF0h4ngN5EdE7XL63vL79tTdRyLNvg8Rsk"
-  APP_ID = 2105857
-  API_HASH = "c8aacc9417551dc98e2b2f61fbfb8387"
-  AUTH_CHANNEL = [-1001463255443, -1001408445549, -1001398883981, -1001275361660, -1001273851623]
-  INDEX_LINK = ""
-  YTDL_COMMAND = "ytdl"
-  LEECH_COMMAND = "leech"
-  CLEAR_THUMBNAIL = "clearthumbnail"
-  SAVE_THUMBNAIL = "savethumbnail"
-  OWNER_ID = 1480779663
-  RCLONE_CONFIG = """type = drive\nscope = drive\ntoken = {"access_token":"ya29.a0AfH6SMB56uBYpLpBCTcERiRt8sRWuQ2H1Zgnei9UKndFCeeyCrXpWbhZuMi02BwyrypdW58wrjmv7PEhRFlYCfk_ewzL5pyuoeI0Hv4gg_NWzRFIBqGDHy6PVebZtLF8jy8yidDH1-R0pt_HZoOv6E_T35lz-Qq4kMXeMe1HrX8","token_type":"Bearer","refresh_token":"1//0gdVePwbuhacKCgYIARAAGBASNwF-L9IriD5ajDaKEEfzdz8oCgbBVOivVTP8TjVfAA5Sfk0Rqn4YhF1kX19Ov0HcW_laIHO5GkQ","expiry":"2020-12-21T09:01:22.303947129Z"}"""
-  GLEECH_COMMAND = "gleech"
-  TELEGRAM_LEECH_COMMAND_G = "tleech"
-  PYTDL_COMMAND_G = "pytdl"
-  MAX_TG_SPLIT_FILE_SIZE = 2097152000
-  CANCEL_COMMAND_G = "cancel"
-  STATUS_COMMAND = "status"
+#Fill your all data, read readme for reference
+
+#FOR CUSTOM COMMANDS READ REAME AND FILL THEM...
+
+class Config(Config):
+    TG_BOT_TOKEN= "1769893770:AAEIvnjKmjwArTZZMJ2TjUek0Fd7_jaoff0"
+    APP_ID = 2105857
+    API_HASH = "c8aacc9417551dc98e2b2f61fbfb8387"
+    OWNER_ID = 1640567687
+    AUTH_CHANNEL = [-1001463255443,1204927413,1640567687]
+    UPLOAD_AS_DOC = "False"
+    LEECH_COMMAND = "leech@CerminStreamRobot"
+    GLEECH_COMMAND = "gleech@CerminStreamRobot" 
+    YTDL_COMMAND = "ytdl@CerminStreamRobot"
+    PYTDL_COMMAND = "pytdl@CerminStreamRobot"
+    INDEX_LINK = "https://nando.xyz/0:"
+    TELEGRAM_LEECH_COMMAND_G = "tleech@CerminStreamRobot"
+    GET_SIZE_G = "getsize@CerminStreamRobot"
+    LOG_COMMAND = "log@CerminStreamRobot"
+    RENEWME_COMMAND = "renewme@CerminStreamRobot"
+    CLONE_COMMAND_G = "gclone@CerminStreamRobot"
+    STATUS_COMMAND = "server@CerminStreamRobot"
+    SAVE_THUMBNAIL = "save@CerminStreamRobot"
+    CLEAR_THUMBNAIL = "clearthumbnail@CerminStreamRobot"
+    DESTINATION_FOLDER = "Leech" #Name of your folder read readme(not id of the folder)
+    #Just don't fill RCLONE_CONFIG vars, insted copy your rclone.conf file in root directory
+    #if your wanted to fill -- fill your rclone config like this(Your config may have some extra value or less. so Don't worry)
+    RCLONE_CONFIG = """
+[TEAM]
+type = drive
+scope = drive
+token = {"access_token":"ya29.a0AfH6SMCJs9VkPcbj_kD-Yzio0ayv2ZMIrjgnVccyyZnyaH-hcTSCCNU51Kgrnqyy3j3CXb2c3Af1ePS_Wt2_Tw2i2gfs31lmnYRA0H8WAa09nIHR_QhHnCN_bH6v_MxZL3yvJl2wcnJL1g-ZFKyteHv78Fpg","token_type":"Bearer","refresh_token":"1//0g86Tbq5gTv3kCgYIARAAGBASNwF-L9IrOS0rHCI3rI5ZhS4hBfq8xqq0Oqn7Rd7uraGZA0aYLuP3BFRGq3du9jUlKKJ2xYw9KVo","expiry":"2021-03-07T22:51:51.479491053Z"}
+team_drive = 0AGXuqNLwr3ISUk9PVA
+"""
+
